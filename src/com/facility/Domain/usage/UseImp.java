a2ac9905e0eb8df5ea7aa8c9f44fb3a2be5586d5
Maha <42777088+MahaBKT@users.noreply.github.com>
2020-02-23 19:56:05 -0600

    Update UseImp.java

--- src/com/facility/Domain/usage/UseImp.java
+++ src/com/facility/Domain/usage/UseImp.java
@@ -1,8 +1,66 @@
 package com.facility.Domain.usage;
 
-
-
-
 public class UseImp implements Usage {
+	private int FacilityUseStart;
+	private int FacilityUseEnd;
+	private int FacilityActualUsage;
+	public int getFacilityUseStart() {
+		return FacilityUseStart;
+	}
+	public void setFacilityUseStart(int facilityUseStart) {
+		FacilityUseStart = facilityUseStart;
+	}
+	public int getFacilityUseEnd() {
+		return FacilityUseEnd;
+	}
+	public void setFacilityUseEnd(int facilityUseEnd) {
+		FacilityUseEnd = facilityUseEnd;
+	}
+	public int getFacilityActualUsage() {
+		return FacilityActualUsage;
+	}
+	public void setFacilityActualUsage(int facilityActualUsage) {
+		FacilityActualUsage = facilityActualUsage;
+	}
+	
+	public String toString() {
+		return "UseImp [ facilityUseStart= " + FacilityUseStart +",facilityUseEnd=" + FacilityUseEnd+ ",facilityActualUsage=" + FacilityActualUsage+"]"; 
+	}
+	@Override
+	public int FacilityUseStart() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int FacilityUseEnd() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int FacilityActualUsage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public void facilityHoursOfOperation(int hoursOfOperation) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void facilityAssignHoursOfOperation(int assignHoursOfOperation) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void vacateFacility() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void facilityList() {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
