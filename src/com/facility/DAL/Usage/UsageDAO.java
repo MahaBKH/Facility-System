a36de3e7184ff2840ffd917451bafb3993d69d51
Maha <42777088+MahaBKT@users.noreply.github.com>
2020-02-23 22:18:12 -0600

    Update UsageDAO.java

--- src/com/facility/DAL/Usage/UsageDAO.java
+++ src/com/facility/DAL/Usage/UsageDAO.java
@@ -1,5 +1,43 @@
 package com.facility.DAL.Usage;
 
+import java.util.ArrayList;
+
+import com.facility.Domain.usage.Usage;
+
 public class UsageDAO {
+
+	public ArrayList<Usage> listActualUsage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<Usage> listInspection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer calcUsageRate(Usage usage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void assignFacilityToUse(Usage usage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void vacateFacility(Usage usage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void isInUseDuringInterval(Usage usage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	
+	
 
 }
