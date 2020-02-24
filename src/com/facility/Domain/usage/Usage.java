1575e55fe7cea66b34415f0f0a4c946036728670
Maha <42777088+MahaBKT@users.noreply.github.com>
2020-02-23 19:55:30 -0600

    Update Usage.java

--- src/com/facility/Domain/usage/Usage.java
+++ src/com/facility/Domain/usage/Usage.java
@@ -1,5 +1,16 @@
 package com.facility.Domain.usage;
 
 public interface Usage {
+	
+	public int FacilityUseStart();
+	public int FacilityUseEnd();
+	public int FacilityActualUsage();
+	public void facilityHoursOfOperation(int hoursOfOperation);
+	public void facilityAssignHoursOfOperation(int assignHoursOfOperation);
+	public void vacateFacility();
+	
+	public void facilityList();
+	
+	public String toString();
 
 }
