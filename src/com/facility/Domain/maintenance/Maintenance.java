b4a0d8b807f1d3eb60a5af99e4e9f6246c69b3e3
Maha <42777088+MahaBKT@users.noreply.github.com>
2020-03-26 17:30:21 -0500

    Update Maintenance.java

--- src/com/facility/Domain/maintenance/Maintenance.java
+++ src/com/facility/Domain/maintenance/Maintenance.java
@@ -1,5 +1,7 @@
 package com.facility.Domain.maintenance;
 
+import java.util.ArrayList;
+
 import com.facility.Domain.facility.Facility;
 import com.facility.Domain.facility.FacilityImp;
 
@@ -10,7 +12,12 @@ public interface Maintenance {
 	public void setFacilityMaintenanceDetails(MaintenanceInspectionImp inspection);
 	public String getFacilityID();
 	public void setFacilityID(String facilityID);
-	
 	public Facility getFacility();
 	public void setFacility(FacilityImp facility);
+	public Integer calcProblemRateForFacility(Facility facility);
+	public Integer calcDownTimeForFacility(Facility facility);
+	public Integer calcMaintenanceCostForFacility(Facility facility);
+	public ArrayList<MaintenanceImp> listFacilityProblems(Maintenance maint);
+	public ArrayList<MaintenanceImp> listMaintRequests(Maintenance maint);
+	public ArrayList<MaintenanceImp> getAllMaintenance();
 }
